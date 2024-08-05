3d6a43f6eaa0dbf8bf8c82d6b9d79bbc2e61e3a4
Niilo Keinänen <niilo.keinanen@lightningchart.com>
2024-08-05 09:40:32 +0300

    Tweak test

--- test/index.js
+++ test/index.js
@@ -1,22 +1,21 @@
-import express from "express";
-import { PNG } from "pngjs";
-import { lightningChart, renderToPNG } from "@lightningchart/lcjs-headless";
-import { flatThemeLight } from "@lightningchart/lcjs-themes";
+const express = require("express");
+const { PNG } = require("pngjs");
+const {
+  lightningChart,
+  renderToPNG,
+} = require("@lightningchart/lcjs-headless");
+const { flatThemeLight } = require("@lightningchart/lcjs-themes");
+const { ColorRGBA, SolidFill } = require("@lightningchart/lcjs");
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  const lc = lightningChart({
-    // Valid until 8/6/2024
-    license:
-      "0002-n6Q3wipxRCPK7PHm1Z1p6PdhNNqeKwClN/s+aUwl3vT54A0lse0e9Ryv9JY4YzR8avVBVjTpcgbR7Q8DS2egZAHn-MEUCIQC87Etz4Jn4NLyctnrTCjVOHKsy7VSxa99DG6arbb1RfgIgZKtGhZpmATUsBZ6fkq834+Sq137gFyv7lsz3OVndGvA=",
-    licenseInformation: {
-      appTitle: "LightningChart JS Trial",
-      company: "LightningChart Ltd.",
-    },
-  });
+  const lc = lightningChart({});
   const chart = lc.ChartXY({ theme: flatThemeLight });
+  chart.setSeriesBackgroundFillStyle(
+    new SolidFill({ color: ColorRGBA(255, 0, 0) })
+  );
   const chartOutput = renderToPNG(chart, 800, 640);
   const outputBuff = PNG.sync.write(chartOutput);
   res.setHeader("Content-Type", "image/png");
